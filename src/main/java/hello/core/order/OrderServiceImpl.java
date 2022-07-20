afbdb05f96c3f593d608c97ee6c654bd44273ea4
dydals918 <dydals918@gmail.com>
2022-07-20 09:41:40 +0900

    basic code

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -2,14 +2,20 @@ package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FIxDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService {
 
+    /*
+        확장 o, 변경 o -> OCP 위반
+        인터페이스 의존 o, 구현 클래스 의존 o -> DIP 위반
+     */
     private static MemberRepository memberRepository = new MemoryMemberRepository();
-    private static DiscountPolicy discountPolicy = new FIxDiscountPolicy();
+//    private static DiscountPolicy discountPolicy = new FIxDiscountPolicy();
+    private static DiscountPolicy discountPolicy = new RateDiscountPolicy();
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
