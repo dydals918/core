ab8399990f19122142fa2a42853a99779c28fa1a
dydals918 <dydals918@gmail.com>
2022-08-04 11:15:54 +0900

    request scope 를 사용하여 로그 남기기(오류 발생)

---
+++ src/main/java/hello/core/common/MyLogger.java
@@ -0,0 +1,40 @@
+package hello.core.common;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import java.util.UUID;
+
+/*
+    Http 요청이 컨테이너에 들어오는 시점에 생성
+    각 Http 요청 하나당 하나 생성
+    소멸시점까지 컨테이너가 관리를 책임지기에 @PreDestroy 가 호출 됨
+ */
+@Component
+@Scope(value = "request")
+public class MyLogger {
+
+    private String uuid;
+    private String requestURL;
+
+    public void setRequestURL(String requestURL) {
+        this.requestURL = requestURL;
+    }
+
+    public void log(String message) {
+        System.out.println("[" + uuid + "]" + "[" + requestURL + "] " + message);
+    }
+
+    @PostConstruct
+    public void init() {
+        uuid = UUID.randomUUID().toString();
+        System.out.println("[" + uuid + "] request scope bean create: " + this);
+    }
+
+    @PreDestroy
+    public void destroy() {
+        System.out.println("[" + uuid + "] request scope bean destroy: " + this);
+    }
+}
---
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -0,0 +1,34 @@
+package hello.core.web;
+
+import hello.core.common.MyLogger;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+
+/*
+    requestURL 을 MyLogger 에 저장하는 부분은 스프링 인터셉터나 서블릿 필터에서 활용하는 것이 좋다
+ */
+@Controller
+@RequiredArgsConstructor
+public class LogDemoController {
+
+    private final LogDemoService logDemoService;
+    private final MyLogger myLogger;
+
+    @RequestMapping("log-demo")
+    @ResponseBody
+    public String logDemo(HttpServletRequest request) {
+        String requestURL = request.getRequestURL().toString();
+        myLogger.setRequestURL(requestURL);
+
+        myLogger.log("controller test");
+        logDemoService.logic("testId");
+
+        return "OK";
+    }
+
+}
